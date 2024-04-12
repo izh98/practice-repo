7e78a629ce7816f7213e4f3c5ca1f228b51eeb54
izh98 <165768319+izh98@users.noreply.github.com>
2024-04-12 16:32:09 +0300

    Update script.groovy

--- script.groovy
+++ script.groovy
@@ -6,9 +6,9 @@ def buildJar() {
 def buildImage() {
     echo "building the docker image..."
     withCredentials([usernamePassword(credentialsId: 'quay-repo', passwordVariable: 'PASS', usernameVariable: 'USER')]) {
-        sh 'docker build -t ihregistry.lab:8443/apps/jenkins-app:1.1 .'
+        sh 'docker build -t ihregistry.lab:8443/apps/jenkins-app:1.2 .'
         sh "echo $PASS | docker login -u $USER --password-stdin ihregistry.lab:8443"
-        sh 'docker push ihregistry.lab:8443/apps/jenkins-app:1.1'
+        sh 'docker push ihregistry.lab:8443/apps/jenkins-app:1.2'
     }
 } 
 
