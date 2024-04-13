fe84d2b8a5a8d6801ae962c741c022dc9863d39d
izh98 <ismail_hamad@jbs.com.jo>
2024-04-13 17:33:00 +0300

    configure version increment

--- script.groovy
+++ script.groovy
@@ -1,14 +1,14 @@
 def buildJar() {
     echo "building the application..."
-    sh 'mvn package'
+    sh 'mvn clean package'
 } 
 
 def buildImage() {
     echo "building the docker image..."
     withCredentials([usernamePassword(credentialsId: 'quay-repo', passwordVariable: 'PASS', usernameVariable: 'USER')]) {
-        sh 'docker build -t ihregistry.lab:8443/apps/jenkins-app:1.2 .'
+        sh "docker build -t ihregistry.lab:8443/apps/jenkins-app:${IMAGE_NAME} ."
         sh "echo $PASS | docker login -u $USER --password-stdin ihregistry.lab:8443"
-        sh 'docker push ihregistry.lab:8443/apps/jenkins-app:1.2'
+        sh "docker push ihregistry.lab:8443/apps/jenkins-app:${IMAGE_NAME}"
     }
 } 
 
